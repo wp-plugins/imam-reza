4c8903b437d8731ecb2529b22448eb1aa720e186
alabdrezvan <alabdrezvan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-14 07:43:23 +0000

    start
    git-svn-id: https://plugins.svn.wordpress.org/imam-reza/trunk@1067709 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Imam Reza.php
@@ -0,0 +1,85 @@
+﻿<?php
+/*
+Plugin Name: Razavi TV
+Description: Online watching Mashhad-Harame emam Reza
+author:Reza nasrollahi in 7learn.com
+Plugin URI: http://7learn.com/
+
+*/
+/* Start Adding Functions Bezamenw this Line */
+
+
+/* Stop Adding Functions Bezamenw this Line */
+?>
+<?php // Creating the widget 
+class zamen_widget extends WP_Widget {
+
+function __construct() {
+parent::__construct(
+// Base ID of your widget
+'zamen_widget', 
+
+// Widget name will appear in UI
+__('زیارت امام رئوف (علیه السلام)', 'zamen_widget_domain'), 
+
+// Widget description
+array( 'description' => __( 'افزونه ی جملات و خاطرات قصار محمد تقی بهجت فومنی', 'zamen_widget_domain' ), ) 
+);
+}
+
+// Creating widget front-end
+// This is where the action happens
+public function widget( $args, $instance ) {
+$title = apply_filters( 'widget_title', $instance['title'] );
+// before and after widget arguments are defined by themes
+echo $args['before_widget'];
+if ( ! empty( $title ) )
+echo $args['before_title'] . $title . $args['after_title'];
+
+// This is where you run the code and display the output
+print __( '
+  <div id="velayattv_wrapper" style="position: relative; width: 100%; height: 100%;">
+    <object type="application/x-shockwave-flash" data="http://shiayan.ir/wp-content/themes/shiayan/swf/player.swf" width="100%" height="100%" bgcolor="#000000" id="velayattv" name="velayattv" tabindex="0">
+      <param name="allowfullscreen" value="true">
+      <param name="allowscriptaccess" value="always">
+      <param name="seamlesstabbing" value="true">
+      <param name="wmode" value="opaque">
+      <param name="flashvars" value="netstreambasepath=http%3A%2F%2Fshiayan.ir%2F%25D9%25BE%25D8%25AE%25D8%25B4-%25D8%25B2%25D9%2586%25D8%25AF%25D9%2587-%25D8%25AD%25D8%25B1%25D9%2585-%25D8%25A7%25D9%2585%25D8%25A7%25D9%2585-%25D8%25B1%25D8%25B6%25D8%25A7-%25D8%25B1%25D9%2588%25D8%25B6%25D9%2587-%25D9%2585%25D9%2586%25D9%2588%25D8%25B1%25D9%2587%2F&amp;id=velayattv&amp;file=flv%3Arozeh2&amp;streamer=rtmp%3A%2F%2F185.23.131.187%2Flive%2F&amp;smoothing=true&amp;menu=false&amp;autostart=true&amp;bufferlength=2&amp;controlbar.position=bottom">
+    </object>
+  </div>
+
+
+', 'zamen_widget_domain' );
+echo $args['after_widget'];
+}
+		
+// Widget Backend 
+public function form( $instance ) {
+if ( isset( $instance[ 'title' ] ) ) {
+$title = $instance[ 'title' ];
+}
+else {
+$title = __( 'زیارت امام رئوف (علیه السلام)', 'zamen_widget_domain' );
+}
+// Widget admin form
+?>
+<p>
+<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+</p>
+<?php 
+}
+	
+// Updating widget replacing old instances with new
+public function update( $new_instance, $old_instance ) {
+$instance = array();
+$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+return $instance;
+}
+} // Class zamen_widget ends here
+
+// Register and zamenad the widget
+function zamen_zamenad_widget() {
+	register_widget( 'zamen_widget' );
+}
+add_action( 'widgets_init', 'zamen_zamenad_widget' );?>
---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== Plugin Name ===
+Contributors: alabdrezvan
+Donate link: http://www.7learn.com/payment/?price=1000
+Tags: Imam Reza,mashhad,امام رضا(علیه السلام)،شمس الشموس،ثامن،پخش آنلاین،زیارت آنلاین،online,islamic,islam,shiye,
+Requires at least: 3.0.1
+Tested up to: 4
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.7learn.com/
+
+افزونه ای برای زیارت آنلاین مرقد مطهر حضرت علی بن موسی الرضاالمرتضی(علیه السلام)
+== Description ==
+
+افزونه ای برای زیارت آنلاین مرقد مطهر حضرت علی بن موسی الرضاالمرتضی(علیه السلام)
+== Installation ==
+
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add it in your widgets
+
+== Frequently Asked Questions ==
+
+= آیا استفاده از افزونه رایگان است ؟ =
+
+بله
+== Screenshots ==
+1. Screen shot (screenshot-1.png) in preview
+== Changelog ==
+
+= 1.0 =
+*این اولین نسخه ی از این قالب می باشد.
+
+== Upgrade Notice ==
+
+= 1.0 =
+انشالله به زودی با تغییرات جدید نمایان خواهد شد...
